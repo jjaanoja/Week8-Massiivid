ddb5974504e2954b958e1515c832d8c6d76df9b0
Janno Jaanoja <jjaanoja@gmail.ee>
2022-02-21 21:19:33 +0200

    add projects

---
+++ FridayNightRandom/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace FridayNightRandom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DisplayRandomFood();
+
+            DisplayRandomDrink();
+
+            DisplayRandomMovie();
+        }
+
+        private static int GenerateRandomIndex(string[] someArray)
+        {
+            Random rnd = new Random();
+
+            int randomIndex = rnd.Next(0, someArray.Length);
+
+            return randomIndex;
+        }
+
+        private static void DisplayRandomFood()
+        {
+            string[] foods = { "chicken wings", "pizza", "sushi", "popcorn", "salad" };
+
+
+            Console.WriteLine($"Computer picked: {foods[GenerateRandomIndex(foods)]}");
+        }
+
+        private static void DisplayRandomDrink()
+        {
+            string[] drinks = { "apple juice", "coke", "fanta", "Sprite", };
+
+
+            Console.WriteLine($"Computer picked: {drinks[GenerateRandomIndex(drinks)]}.");
+        }
+
+        private static void DisplayRandomMovie()
+        {
+            string[] movies = { "Transformers", "Harry Potter", "Uncharted", "Jumanij", "Jungle Ride" };
+
+
+            Console.WriteLine($"Computer picked: {movies[GenerateRandomIndex(movies)]}.");
+        }
+    }
+}
